18b72984e28ab307ca7f419e584bb8c31df960ff
Vincent Udén <vincentuden@gmail.com>
2026-01-06 19:29:26 +0100

    Add env flag for db path

--- composeApp/src/jvmMain/kotlin/com/vincentuden/financeanalyzer/AppDatabase.desktop.kt
+++ composeApp/src/jvmMain/kotlin/com/vincentuden/financeanalyzer/AppDatabase.desktop.kt
@@ -4,10 +4,24 @@ import AppDatabase
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import java.io.File
+import java.nio.file.Paths
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
-    val dbFile = File(System.getProperty("java.io.tmpdir"), "my_room.db")
-    println("Using db file at ${dbFile.absolutePath}")
+    val dbPath = System.getenv("FINANCE_ANALYZER_DB_PATH")
+    val dbFile = if (dbPath != null) {
+        File(dbPath).also { it.parentFile?.mkdirs() }
+    } else {
+        val userHome = System.getProperty("user.home")
+        val os = System.getProperty("os.name").lowercase()
+        val configDir = when {
+            os.contains("linux") -> Paths.get(userHome, ".config", "financeAnalyzer")
+            os.contains("windows") -> Paths.get(userHome, "AppData", "Roaming", "financeAnalyzer")
+            os.contains("mac") -> Paths.get(userHome, "Library", "Application Support", "financeAnalyzer")
+            else -> Paths.get(userHome, ".config", "financeAnalyzer") // fallback to linux-like
+        }.toString()
+        File(configDir, "data.db").also { it.parentFile.mkdirs() }
+    }
+    println("Using db file at ${dbFile.absolutePath}" + if (dbPath != null) " (from env var)" else " (default path)")
     return Room.databaseBuilder<AppDatabase>(
         name = dbFile.absolutePath
     )
